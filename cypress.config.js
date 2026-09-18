322a13e5e64a499ca60d3d7fcf877d80dd07988d
Nandana Nambiar <nnambiarc2017@gmail.com>
2026-09-18 14:58:15 +0530

    Updated report folder

--- cypress.config.js
+++ cypress.config.js
@@ -7,6 +7,7 @@ module.exports = defineConfig({
   screenshotOnRunFailure: true,
   reporter: 'cypress-mochawesome-reporter',
   reporterOptions: {
+    reportDir: 'cypress/reports/html',
     charts: true,
     reportPageTitle: 'SauceDemo Cypress Automation',
     embeddedScreenshots: true,
